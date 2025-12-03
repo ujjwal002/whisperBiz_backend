5962e7d1fe942726f7e44565fd76ef95457a16dd
Ujjwal Kumar <ujjwalsinghrajput2000@gmail.com>
2025-12-03 23:13:03 +0530

    solved the ts error in jwt.ts

--- src/libs/jwt.ts
+++ src/libs/jwt.ts
@@ -1,22 +1,30 @@
-import jwt from "jsonwebtoken";
+import * as jwt from "jsonwebtoken";
 import { Env } from "../config/env";
 
 export const signAccessToken = (payload: any) => {
-  return jwt.sign(payload, Env.JWT_ACCESS_TOKEN_SECRET, {
-    expiresIn: Env.ACCESS_TOKEN_EXPIRES_IN,
-  });
+  return jwt.sign(
+    payload as string | Buffer | jwt.JwtPayload,
+    Env.JWT_ACCESS_TOKEN_SECRET as jwt.Secret,
+    {
+      expiresIn: Env.ACCESS_TOKEN_EXPIRES_IN as jwt.SignOptions["expiresIn"],
+    }
+  ) as string;
 };
 
 export const signRefreshToken = (payload: any) => {
-  return jwt.sign(payload, Env.JWT_REFRESH_TOKEN_SECRET, {
-    expiresIn: Env.REFRESH_TOKEN_EXPIRES_IN,
-  });
+  return jwt.sign(
+    payload as string | Buffer | jwt.JwtPayload,
+    Env.JWT_REFRESH_TOKEN_SECRET as jwt.Secret,
+    {
+      expiresIn: Env.REFRESH_TOKEN_EXPIRES_IN as jwt.SignOptions["expiresIn"],
+    }
+  ) as string;
 };
 
 export const verifyAccessToken = (token: string) => {
-  return jwt.verify(token, Env.JWT_ACCESS_TOKEN_SECRET);
+  return jwt.verify(token, Env.JWT_ACCESS_TOKEN_SECRET as jwt.Secret) as any;
 };
 
 export const verifyRefreshToken = (token: string) => {
-  return jwt.verify(token, Env.JWT_REFRESH_TOKEN_SECRET);
+  return jwt.verify(token, Env.JWT_REFRESH_TOKEN_SECRET as jwt.Secret) as any;
 };
